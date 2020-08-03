ebbe8ed61bb751cd5d7ba1ecf5293ed6e7bc1abf
Andres <ababino@gmail.com>
2020-08-03 17:37:59 -0400

    remove old comments

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-#!flask/bin/python
-
 from flask import Flask
 from flask_cors import CORS
 from apis import api
 
 app = Flask(__name__)
-#cors = CORS(app, resources={r"/foo": {"origins": "http://***REMOVED***"}})
 #cors = CORS(app)
 api.init_app(app)
 
 
 if __name__=='__main__':
-	#app.run(debug=True, port=5001, host='0.0.0.0')
         app.run()
-	#app.run(debug=True, port=5001)
