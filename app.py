ab0191c9bf5eb1d87996260d12e6b958e7c2ed74
Andres <ababino@gmail.com>
2020-08-03 18:08:35 -0400

    add CORS to app

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask_cors import CORS
 from apis import api
 
 app = Flask(__name__)
-#cors = CORS(app)
+cors = CORS(app)
 api.init_app(app)
 
-
 if __name__=='__main__':
-        app.run()
+    app.run()
